a903a575f568540084635cd1a229e7a5535d36d9
Rafael Costa <rafael.costa.ti@gmail.com>
2025-07-20 14:29:49 -0300

    Fix TeamsApi to use @Path annotation for team_id in getTeamDetails

--- data/src/main/java/com/orafaelsc/cstvfuze/data/remote/TeamsApi.kt
+++ data/src/main/java/com/orafaelsc/cstvfuze/data/remote/TeamsApi.kt
@@ -3,11 +3,11 @@ package com.orafaelsc.cstvfuze.data.remote
 import com.orafaelsc.cstvfuze.data.remote.dto.TeamDto
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface TeamsApi {
-    @GET("csgo/teams/{team_id}")
+    @GET("teams/{team_id}")
     suspend fun getTeamDetails(
-        @Query("team_id") teamId: String
+        @Path("team_id") teamId: String
     ): Response<TeamDto>
 }
